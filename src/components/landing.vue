bdbe841ef9d4dfe58e4d42a1ad970dc2e75e2dd2
ligwol <yulia-aur@mail.ru>
2021-06-20 16:40:12 +0300

    header raw

--- src/components/landing.vue
+++ src/components/landing.vue
@@ -1,38 +1,35 @@
 <template>
-    <h1>
-        hello
-        <h2>
-            world
-        </h2>
-    </h1>
-    <header class="hder">
-        <nav class="hder__nav">
-           <a href="#">
-               <img class="hder__logo" src="../../images/logo.png" alt="">
-           </a>
-           <a href="#">
-               <i class="fas fa-home"></i>
-               home
-            </a>
-           <a href="#our-technologies">our technologies</a>
-           <a href="#contakts">contakts</a>
-           <a href="call">
-                +422 2222222
-                <i class="fas fa-phone"></i>   
-            </a>
-        </nav>
-
-    </header>
+    <div class="first-page">
+        <header class="hder">
+            <div class="hder__menu">
+                <a href="#">
+                    <img class="hder__logo" src="../../images/logo.png" alt="">
+                </a>
+                <nav class="hder__nav">
+                    <a href="#">
+                        <i class="fas fa-home"></i>
+                        home
+                    </a>
+                    <a href="#about-us">about us</a>
+                    <a href="#our-technologies">our technologies</a>
+                    <a href="#contakts">contakts</a>
+                </nav>
+            </div>
+            <div class="hder__phone">
+                <p>+422 2222222</p> <i class="fas fa-phone"></i>
+            </div>
+        </header>
+    </div>
     <main>
     </main>
 </template>
 
 <script>
-export default{
-    name: "landing"
-}
+    export default {
+        name: "landing"
+    }
 </script>
 
-<style lang="scss" scoped>
-    @import url(../assets/style.scss);
+<style lang="scss">
+    @import "./../assets/style.scss";
 </style>
